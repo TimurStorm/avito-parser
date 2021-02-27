deb34cb9dbe30607939f5d875d242d89e708d095
Tim <71645120+TimurStorm@users.noreply.github.com>
2021-02-27 22:46:10 +0300

    First function update

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import eel
+from view import find_new_ads
 from settings import *
 
 eel.init(DIR_PATH)
 eel.start(DIR_PATH + "\main.html", geometry={'size': WIND_SIZE, 'position': WIND_SIZE})
-
-
-
