bec7c5d33af5c8470648becf44db848283f29951
Lil-Graver <grvr.acc@gmail.com>
2021-03-20 01:07:25 +0300

    Pain
    Check ->new Story -> Main.html

---
+++ templates/Боль/новая история/script.js
@@ -0,0 +1,14 @@
+new Vue({
+    el:"#app",
+    data:{
+        color:'red',
+        items: [
+            "Home",
+            "About",
+            "Blog",
+            "Map",
+            "Contacts"
+        ],
+        show:false,
+    },
+});
