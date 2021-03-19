df9926f27b93d61f3de21e893bdab0dc9226c120
Lil-Graver <grvr.acc@gmail.com>
2021-03-20 01:23:52 +0300

    Revert "Merge branch 'main' of https://github.com/TimurStorm/avito-parser into main"
    This reverts commit e9fda19f95fe0c1fa33fa99b583e101665aee375, reversing
    changes made to bec7c5d33af5c8470648becf44db848283f29951.

--- app/models.py
+++ app/models.py
@@ -1,22 +1,16 @@
+import requests
 import csv
 import random
 import eel
-from time import sleep
+from time import time, sleep
+from bs4 import BeautifulSoup
+from datetime import datetime
 from .settings import DIR_PATH, VK
-
-
-def send_message(text):
-    VK.messages.send(
-        user_id=443194153,
-        message=text,
-        random_id=random.randint(-2147483648, +2147483647),
-    )
-    sleep(0.5)
 
 
 class Avito_parser:
     def __init__(self, pk, title, url, it, status="not active"):
-        self.id = int(pk)  # индекс
+        self.id = int(pk)
         self.title = title.strip()  # название
         self.url = url  # ссылка
         self.ads = []  # объявления
@@ -26,6 +20,10 @@ class Avito_parser:
             DIR_PATH + "\\csv\\" + self.title + "_ads.csv"
         )  # файл для хранения данных
 
+    def create_ads_file(self):
+        with open(self.ads_file, "w"):
+            self.find_new_ads(mode=False)
+
     def write_parser(self):
         with open("../csv/parsers.csv", "a", encoding="utf-8") as file:
             writer = csv.writer(file, delimiter=",")
@@ -34,20 +32,63 @@ class Avito_parser:
     def __str__(self):
         return "Парсер " + self.title
 
+    # функция отправки сообщения
+    def send_message(self, text):
+        VK.messages.send(
+            user_id=443194153,
+            message=text,
+            random_id=random.randint(-2147483648, +2147483647),
+        )
+        sleep(0.5)
+
+    # находит объявления на сайте
+    def get_all_ads(self, url):
+        start_time = time()
+        page = requests.get(url)
+        code_dict = str(page)
+        code = ""
+        print(f'Get html: {time() - start_time}')
+        start_time = time()
+        for i in code_dict:
+            if i.isdigit():
+                code += i
+        print(f'Code to int: {time() - start_time}')
+        start_time = time()
+        resp = (
+            self.title
+            + " "
+            + datetime.now().strftime("%d %B, %H:%M")
+            + ": "
+            + "Response code "
+            + code
+        )
+
+        soup = BeautifulSoup(page.content, "lxml")
+
+        all_ads = soup.find("div", attrs={"data-marker": "catalog-serp"})
+        count = soup.find('span', attrs={"data-marker": "page-title/count"}).get_text().replace(u'\xa0','').replace(u'\u2009', '')
+        count = int(count.strip())
+        print(f"Количество объявлений {count}")
+        ad = all_ads.find_all("div", attrs={"data-marker": "item"})
+        print(f'Soup: {time() - start_time}')
+        return ad, resp, count
+
     # проверяет список объявлений на наличие новых предложений и в случае обнаружения присылает уведомление в лс в вк
-    def find_new_ads(
-        self,
-        mode=True,
-        new_ad=None,
-    ):
-
+    def find_new_ads(self, mode=True):
+        print("----------" + self.title + "----------")
         ads_file = self.ads_file
+        url = self.url
         ads_title = self.ads
-
         with open(ads_file, encoding="utf-8") as file:
             reader = csv.reader(file, delimiter=",")
             for row in reader:
-                ads_title.append(row[1])
+                ads_title.append(row[0])
+        start_time = time()
+        new_ad, resp, count = self.get_all_ads(url=url)
+        if count - len(ads_title) > 0:
+            print(f'{count - len(ads_title)} Новых объявлений!')
+        print(f"Get uls {time() - start_time}")
+        start_time = time()
         with open(ads_file, mode="a", encoding="utf-8") as file:
             writer = csv.writer(
                 file,
@@ -58,25 +99,29 @@ class Avito_parser:
             for ad in new_ad:
                 # ищем информацию о каждом объявлении
                 info = ad.find("div", attrs={"class": "iva-item-body-NPl6W"})
+                print(f"Find info about new ad: {time() - start_time}")
+                start_time = time()
                 title_link = info.find("a", attrs={"data-marker": "item-title"})
+                print(f"Find title-link about new ad: {time() - start_time}")
+                start_time = time()
                 title = title_link.find("h3").get_text()
-                link = "https://www.avito.ru" + title_link.get("href")
-
-                new = Ad(title, link, 1)
+                print(f"Find title about new ad: {time() - start_time}")
+                start_time = time()
+                new = Ad(title, url, 1)
 
                 # если объявления нет в списке
-                if new.url not in ads_title:
-
+                if new.title not in ads_title:
+                    link = "https://www.avito.ru" + title_link.get("href")
                     ads_title.append(new)
                     # записываем в файл новое объявление
                     writer.writerow([new.title, new.url])
                     if mode:
                         # присылает уведомление в ВК
-                        send_message(
-                            text="Новое объявление!" + "\n" + title + "\n" + link
-                        )
+                        self.send_message(text="New Book!" + "\n" + title + "\n" + link)
                         # выводит уведомление в приложении
                         eel.print("New Ad!" + "\n" + title + "\n" + link)
+
+            eel.print(resp)
 
 
 class Ad:
--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 import vk_api
 from pathlib import Path
@@ -10,7 +9,3 @@ DIR_PATH = str(Path(os.getcwd()).parent)
 token = "56036fde45f5c4e10920de4ad5d1b7c6bf4f78feb37395313eec5fa2acf991d43a7f2ae5ca0f4f9688213"
 session = vk_api.VkApi("89172461399", "000168154Tim", token=token)
 VK = session.get_api()
-
-ACTIVE_PARSERS = []
-
-MAIN_LOOP = asyncio.get_event_loop()
